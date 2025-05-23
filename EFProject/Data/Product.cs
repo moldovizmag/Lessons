7b7d77f63f0bdb089b778360f2264802fba27d87
Alex <moldovizmag@gmail.com>
2025-05-23 21:26:06 +0300

    Add Stock field and migration

--- EFProject/Data/Product.cs
+++ EFProject/Data/Product.cs
@@ -1,9 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
+    [Precision(18, 2)]
     public decimal Price { get; set; }
     public int Stock { get; set; }
     public int CategoryId { get; set; }
--- EFProject/obj/Debug/net9.0/EFProject.AssemblyInfo.cs
+++ EFProject/obj/Debug/net9.0/EFProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217135889d30fea1e20a772d6531bd08e7899858")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4989855aafc970e2cdcb33486d28dea4a09d24ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
