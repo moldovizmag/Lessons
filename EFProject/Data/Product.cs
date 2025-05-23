2da08ef0dc501d1ce64858ff6e2c17c442d4f4c2
Alex <moldovizmag@gmail.com>
2025-05-23 21:28:40 +0300

    Add Stock field and migration

--- EFProject/Data/Product.cs
+++ EFProject/Data/Product.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 public class Product
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty;
     [Precision(18, 2)]
     public decimal Price { get; set; }
