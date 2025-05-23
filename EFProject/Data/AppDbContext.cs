217135889d30fea1e20a772d6531bd08e7899858
Alex <moldovizmag@gmail.com>
2025-05-23 20:26:07 +0300

    Version 2

---
+++ EFProject/Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite("Data Source=product.db");
+}
---
+++ EFProject/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+using System.Runtime.Intrinsics.Arm;
+
+using (var db = new AppDbContext())
+{
+    db.Database.EnsureCreated();
+    NameSearch(db, "Яблоки");
+}
+
+
+static void AddProduct(AppDbContext db, string name, decimal price)
+{
+    db.Products.Add(new Product { Name = name, Price = price });
+    db.SaveChanges();
+}
+
+static void ListProducts(AppDbContext db)
+{
+    foreach (var product in db.Products)
+        Console.WriteLine($"{product.Id}: {product.Name} - {product.Price:C}");
+}
+
+static void UpdatePrice(AppDbContext db, int id, decimal price)
+{
+    var product = db.Products.Find(id);
+    product.Price = price;
+    db.SaveChanges();
+}
+
+static Product NameSearch(AppDbContext db, string name)
+{
+    Product product = db.Products.Where(p => p.Name.Contains(name)).First();
+    Console.WriteLine($"{product.Name} - {product.Price:C}");
+    return product;
+}
