4989855aafc970e2cdcb33486d28dea4a09d24ec
Alex <moldovizmag@gmail.com>
2025-05-23 21:17:30 +0300

    BegoreCodReview

--- EFProject/Data/AppDbContext.cs
+++ EFProject/Data/AppDbContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     => options.UseSqlite("Data Source=product.db");
 }
